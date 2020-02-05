7b582d039bffa4378b2982739378428f373b8fa0
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-02-05 15:31:11 +0100

    draw and generate new cells and new layers

--- typescript/Board.ts
+++ typescript/Board.ts
@@ -1,83 +1,98 @@
 class Board {
-    //Class attributes
-    public layout: Array<Array<number>> = [
-        [0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0],
-        [0, 1, 1, 1, 0],
-        [0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0],
-    ]
+  //Class attributes
+  public layout: Array<Array<number>> = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
+  ];
 
-    //Class constructor
-    constructor() {
+  //Class constructor
+  constructor() {}
 
+  //Class methods
+
+  generateCells() {
+    // const activeCells: Array<Cell> = [];
+
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        //generate cells
+        let size = 50;
+        let xPos = j * size;
+        let yPos = i * size;
+
+        if (this.layout[i][j] === 0) {
+          let voidedCell = new Cell(xPos, yPos, size, 0);
+          voidedCell.draw();
+        }
+        if (this.layout[i][j] === 1) {
+          let emptyCell = new Cell(xPos, yPos, size, 1);
+          emptyCell.draw();
+        }
+      }
     }
+  }
 
-    //Class methods
+  addCellLayer() {
+    // let generation: Array<number> = [];
 
-    generateCells() {
-        for (let i = 0; i > this.layout.length; i++) {
-            for (let j = 0; j > this.layout.length; i++) {
-                //generate cells
+    this.layout.push([]);
+    this.layout.unshift([]);
+
+    this.layout.forEach(arrayOfCells => {
+      console.log(arrayOfCells);
+
+      if (arrayOfCells.length != this.layout.length) {
+        if (arrayOfCells.length === 0) {
+          for (let i = 0; i < this.layout.length; i++) {
+            arrayOfCells.push(0);
+          }
+        } else {
+          arrayOfCells.unshift(0);
+          arrayOfCells.push(0);
+        }
+      }
+    });
+
+    this.addEmptyCell()
+  }
+
+  addEmptyCell() {
+    let voidedCells: Array<Array<number>> = this.countVoidedCells();
+    const rng: number = Math.floor(random(voidedCells.length));
+    const coordinates: Array<number> = voidedCells[rng];
+
+    console.log(coordinates);
+    console.log(voidedCells);
+    console.log(rng);
+
+    if (voidedCells.length != 0) {
+      for (let i = 0; i < this.layout.length; i++) {
+        if (coordinates[0] === i) {
+          for (let j = 0; j < this.layout.length; j++) {
+            if (coordinates[0] === i && coordinates[1] === j) {
+              this.layout[coordinates[0]][coordinates[1]] = 1;
             }
+          }
         }
+      }
+    } else if (voidedCells.length === 0) {
+      this.addCellLayer();
+      voidedCells = this.countVoidedCells();
     }
+  }
 
-    addCellLayer() {
-        let generation: Array<number> = []
-
-        for (let i = 0; i < this.layout.length; i++) {
-            generation.push(0)
+  countVoidedCells() {
+    let arrayOfVoidedCells: Array<Array<number>> = [];
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        if (this.layout[i][j] === 0) {
+          arrayOfVoidedCells.push([i, j]);
         }
-        
-        this.layout.unshift(generation)
-        this.layout.push(generation)
-        
-        this.layout.forEach(arrayOfCells => {
-            if (arrayOfCells.length != this.layout.length) {
-                arrayOfCells.unshift(0)
-                arrayOfCells.push(0)
-            }
-        });
-        // console.log(this.layout);
-        console.log(this.layout);  
+      }
     }
-
-    addEmptyCell() {
-
-        let voidedCells: Array<Array<number>> = this.countVoidedCells()
-
-        if (voidedCells.length === 0) {
-            this.addCellLayer()
-            voidedCells = this.countVoidedCells()
-        }
-
-        let rng = Math.floor(random(voidedCells.length))        
-        for (let i = 0; i < this.layout.length; i++) {
-            if(voidedCells[rng][0] === i) {                
-                for (let j = 0; j < this.layout.length; j++) {
-                    if (voidedCells[rng][1] === j) {
-                        this.layout[i][j] = 1                        
-                    }
-                }
-            }
-        }
-
-        
-
-        console.log(voidedCells, this.layout);
-        
-    }
-
-    countVoidedCells() {
-        let arrayOfVoidedCells: Array<Array<number>> = []
-        for (let i = 0; i < this.layout.length; i++) {
-            for (let j = 0; j < this.layout.length; j++) {
-                if (this.layout[i][j] === 0 ) {
-                    arrayOfVoidedCells.push([i, j])
-                }
-            }
-        }
-        return arrayOfVoidedCells
-    }
+    return arrayOfVoidedCells;
+  }
 }
