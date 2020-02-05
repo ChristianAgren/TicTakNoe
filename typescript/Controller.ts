53949dfc412152fb2bb328b75383a7229a8b8959
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-02-05 18:11:16 +0100

    added logic to controller

--- typescript/Controller.ts
+++ typescript/Controller.ts
@@ -1,13 +1,42 @@
 class Controller {
     //Class attributes
-    
-    // Skapa spelbräde och hantera spelare
-    // Hantera positionering av spelkort?
+    private scene: Scene
+    private board: Board
+    public cameraX: number
+    public cameraY: number
+    private distX: number
+    private distY: number
+    private margin: number
     
     //Class constructor
     constructor() {
-
+        this.scene = {
+            width: windowWidth,
+            height: windowHeight
+        }
+        this.board = new Board()
+        this.cameraX = 0
+        this.cameraY = 0
+        this.distX = 0
+        this.distY = 0
+        this.margin = 30
     }
 
     //Class functions
+    public update() {
+        this.distX = (this.scene.width/2 - mouseX)*.01
+        this.distY = (this.scene.height/2 - mouseY)*.01
+        if (mouseX+this.margin < this.scene.width/2 ||
+            mouseX-this.margin > this.scene.width/2) {
+                this.cameraX+=this.distX                
+            }
+            if (mouseY+this.margin < this.scene.height/2 ||
+                mouseY-this.margin > this.scene.height/2) {
+                    this.cameraY+=this.distY                 
+                }
+            }
+
+    public draw() {
+
+    }
 }
