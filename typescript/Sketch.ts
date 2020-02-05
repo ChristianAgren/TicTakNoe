3d2cc40a09ddd03995fd3f0b6e94e56cc988202f
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-02-05 18:33:57 +0100

    small tweaks

--- typescript/Sketch.ts
+++ typescript/Sketch.ts
@@ -3,21 +3,17 @@ window.addEventListener("resize", setup);
 let controller: Controller
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   frameRate(60)
-  fullscreen()
-  controller = new Controller()  
+  rectMode(CENTER)
+  controller = new Controller()
+  background(205, 55, 55)
 }
 
 function draw() {
   let fps = frameRate()
   console.log(fps);
-  
   // translate(controller.cameraX, controller.cameraY)
-  background(205, 55, 55)
-  fill(255)
-  rectMode(CENTER)
-  rect(windowWidth/2, windowHeight/2, 300, 300)
-  // controller.update()
-  // controller.update()
+  controller.update()
+  controller.draw()
 }
