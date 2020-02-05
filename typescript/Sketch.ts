4284fad59abf9b882f7b135ec4b7e52fefbede24
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-02-05 18:10:40 +0100

    added controller in sketch.ts

--- typescript/Sketch.ts
+++ typescript/Sketch.ts
@@ -1,14 +1,23 @@
 //This is the main .ts that runs on start-up
 window.addEventListener("resize", setup);
-let test = new Board()
+let controller: Controller
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  console.log(test.layout);
-  
+  frameRate(60)
+  fullscreen()
+  controller = new Controller()  
 }
 
 function draw() {
-  background(205, 55, 55);
-  // noStroke();
+  let fps = frameRate()
+  console.log(fps);
+  
+  // translate(controller.cameraX, controller.cameraY)
+  background(205, 55, 55)
+  fill(255)
+  rectMode(CENTER)
+  rect(windowWidth/2, windowHeight/2, 300, 300)
+  // controller.update()
+  // controller.update()
 }
