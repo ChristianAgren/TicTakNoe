d31ebaf0d143a8ace6e730c263c2235f973e912b
Fred Malm <55448542+FredrikMalmborg@users.noreply.github.com>
2020-02-06 15:10:55 +0100

    Horizontal & vertical win check

--- typescript/Board.ts
+++ typescript/Board.ts
@@ -1,16 +1,19 @@
 class Board {
   //Class attributes
   public layout: Array<Array<number>> = [
+    [1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0]
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1]
   ];
   private cellArray: Array<Cell> = [];
+  private isWin: boolean;
+
   //Class constructor
   constructor() {
     this.generateCells();
+    this.isWin = false;
   }
 
   //Class methods
@@ -97,5 +100,64 @@ class Board {
       }
     }
     return arrayOfVoidedCells;
+  }
+
+  private checkWin() {
+    // this.horizontal();
+    // this.vertical();
+    this.diagonal();
+
+    console.log(this.isWin);
+  }
+
+  private horizontal(): boolean {
+    let index: number = 1;
+    this.layout.forEach(row => {
+      if (row.join("").includes("111") && !this.isWin) {
+        console.log("O won" + "\n" + "on row : " + index);
+        this.isWin = true;
+      } else if (row.join("").includes("000") && !this.isWin) {
+        console.log("X won" + "\n" + "on row : " + index);
+        this.isWin = true;
+      } else if (!this.isWin) {
+        console.log("No win HOR!");
+      }
+      index++;
+    });
+    return this.isWin;
+  }
+
+  private vertical() {
+    let column: Array<number> = [];
+
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        column.push(this.layout[j][i]);
+      }
+      if (column.join("").includes("111") && !this.isWin) {
+        console.log("O won" + "\n" + "on column : " + (i + 1));
+        this.isWin = true;
+      } else if (column.join("").includes("000") && !this.isWin) {
+        console.log("X won" + "\n" + "on column : " + (i + 1));
+        this.isWin = true;
+      } else if (!this.isWin) {
+        console.log("No win VER!");
+      }
+      column = [];
+    }
+  }
+
+  private diagonal() {
+    let diagonal: Array<number> = [];
+    let index: number = 0;
+    for (let i = 0; i < this.layout.length; i++) {
+      for (let j = 0; j < this.layout.length; j++) {
+        diagonal.push(this.layout[j][j + index]);
+        console.log(index);
+      }
+      index++;
+      console.log(diagonal);
+      diagonal = [];
+    }
   }
 }
